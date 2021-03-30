fca38abfcb4aa6e90d9dced74f346f216f0dd38d
Fakher Meddeb <fakher.meddeb@itsfactory.fr>
2021-03-31 01:03:27 +0200

    - configure setup file

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ fi
 apt update --yes
 apt dist-upgrade --yes
 apt upgrade --yes
-apt install --yes git python3 python3-pip python3-dev libpython-dev libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
-echo "############### LINUX DEPENDENCIES DONE - CHECK FOR ERRORS ###############"
-exec pip install -r requirements.txt
-echo "############### PYTHON DEPENDENCIES DONE - CHECK FOR ERRORS ###############"
+apt install --yes git python3 python3-pip python3-requests python3-http-parser python3-hue-py python3-dev libpython-dev libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
+echo "############### BASE DEPENDENCIES DONE - CHECK FOR ERRORS ###############"
+apt --yes install python3-opencv
+echo "############### OPENCV DONE - CHECK FOR ERRORS ###############"
 echo "############### FULLY COMPLETE - CHECK FOR ERRORS ###############"
 exit
