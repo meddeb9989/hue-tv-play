425b2e2c4727c7f9bf2f3ecf2254ede5dc4b290a
Fakher Meddeb <fakher.meddeb@itsfactory.fr>
2021-04-02 16:29:08 +0200

    - remove sleep after color send

--- play.py
+++ play.py
@@ -318,7 +318,7 @@ def send_colors_to_lights():
         for light, (hue, saturation) in rgb_colors.items():
             light.set_state({'hue': hue, 'sat': saturation})
         buffer_lock.release()
-        time.sleep(.01)  # 0.01 to 0.02 (slightly under 100 or 50 messages per sec // or (.015 = ~66.6))
+        # time.sleep(.01)  # 0.01 to 0.02 (slightly under 100 or 50 messages per sec // or (.015 = ~66.6))
 
 
 ####################################
